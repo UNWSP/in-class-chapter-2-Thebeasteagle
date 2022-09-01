aeff0fe73a6d1baf85435452d932d99bae5bdcd7
Nathan <97567424+Thebeasteagle@users.noreply.github.com>
2022-09-01 18:35:24 -0500

    In class average program
    Adds 5 subjects, and finds the average.

---
+++ src/chap2/Chapter2a.java
@@ -0,0 +1,25 @@
+package chap2;
+
+public class Chapter2a {
+
+	public static void main(String[] args) {
+		//Define 5 subjects 
+	    int num1 = 10;
+	    int num2 = 15;
+	    int num3 = 20;
+	    int num4 = 25;
+	    int num5 = 35;
+	    
+	    //Define average variable
+	    float avg = 0;
+
+	    // calculate the sum value
+	    int sum = num1 + num2 + num3 + num4 +num5;
+	    // calculate the average value
+	    avg = sum/5;
+
+	    // display result
+	    System.out.println("Average: " + avg );
+	}
+
+}
